908314c70650e89eaecc1fb0ad8204f15e0e1f87
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-18 16:29:34 +0600

    toys data is inserting and getting from  mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express()
@@ -13,30 +14,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // Connect the client to the server	(optional starting in v4.7)
+        const toyCollection = client.db('AnimeToys').collection('Toys')
+
+        app.post('/allToys', async (req, res) => {
+            const body = req.body;
+            const result = await toyCollection.insertOne(body)
+            res.send(result)
+        })
+        app.get('/allToys', async (req, res) => {
+            const result = await toyCollection.find().toArray();
+            res.send(result)
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (error) {
-      console.log(error.message)
+        console.log(error.message)
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('Toys data Here')
-  })
-  
-  app.listen(port, (req, res) => {
-  
-  })
+})
+
+app.listen(port, (req, res) => {
+
+})
