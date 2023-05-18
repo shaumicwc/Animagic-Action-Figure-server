18936221c444f3bf3e99d0040fe73b42706919ad
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-18 16:23:25 +0600

    mongodb setup complete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express()
@@ -5,6 +6,32 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kfci0x.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Toys data Here')
