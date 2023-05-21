f1c39144ac9b7813b4ffef06f56198ba20643f6c
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-21 15:56:13 +0600

    updated and vercel deployed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kfci0x.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongodbClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
